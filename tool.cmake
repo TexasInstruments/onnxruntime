f145bec7bee26b9dfa43b3e07645ee1a5f8b8140
Abhay Chirania <a-chirania@ti.com>
2024-11-18 16:56:16 +0530

    Update tool.cmake for 10.1 SDK toolchain update
    Signed-off-by: Abhay Chirania <a-chirania@ti.com>

--- tool.cmake
+++ tool.cmake
@@ -1,10 +1,14 @@
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR aarch64)
-SET(tools /home/a0133185/ti/GIT_C7x_MMA_TIDL/c7x-mma-tidl/ti_dl/release/build_cloud/test/gcc-arm-9.2-2019.12-x86_64-aarch64-none-linux-gnu)
 SET(CMAKE_SYSTEM_VERSION 1)
-SET(CMAKE_C_COMPILER ${tools}/bin/aarch64-none-linux-gnu-gcc)
-SET(CMAKE_CXX_COMPILER ${tools}/bin/aarch64-none-linux-gnu-g++)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Set the gcc and g++ toolchain
+SET(sdk_path /home/abhay/ti-firmware-builder-j721s2-evm-10_01_00_01)
+SET(CMAKE_C_COMPILER  ${sdk_path}/toolchain/sysroots/x86_64-arago-linux/usr/bin/aarch64-oe-linux/aarch64-oe-linux-gcc)
+SET(CMAKE_CXX_COMPILER  ${sdk_path}/toolchain/sysroots/x86_64-arago-linux/usr/bin/aarch64-oe-linux/aarch64-oe-linux-g++)
+SET(CMAKE_SYSROOT ${sdk_path}/targetfs)
+include_directories (${sdk_path}/targetfs/usr/lib/python3.12/site-packages/numpy/core/include)
